eaa410d819c85e745865254f8941e2ca55ce73db
Даниил <64002850+Odessey18@users.noreply.github.com>
2022-04-23 17:59:58 +0300

    Initial commit

---
+++ Quizer/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizer
+{
+    public class Class1
+    {
+        public void Show()
+        {
+            System.Windows.Forms.MessageBox.Show("dll");
+            int r = 0;
+        }
+    }
+}
